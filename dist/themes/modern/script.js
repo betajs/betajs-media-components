45f4cd2e79290aae5851ed0a2e4f796ce4d2c244
Oliver Friedmann <oliver.friedmann@gmail.com>
2019-01-11 16:22:36 -0500

    Merge

--- dist/themes/modern/script.js
+++ dist/themes/modern/script.js
@@ -1,5 +1,5 @@
 /*!
-betajs-media-components - v0.0.147 - 2018-12-30
+betajs-media-components - v0.0.150 - 2019-01-11
 Copyright (c) Ziggeo,Oliver Friedmann,Rashad Aliyev
 Apache-2.0 Software License.
 */

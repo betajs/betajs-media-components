5e4f8ecf45ad8253f7db27ca9c06e35ab851ca43
Oliver Friedmann <oliver.friedmann@gmail.com>
2020-01-17 17:20:39 -0500

    Merge

--- dist/themes/elevate/script.js
+++ dist/themes/elevate/script.js
@@ -1,5 +1,5 @@
 /*!
-betajs-media-components - v0.0.203 - 2020-01-08
+betajs-media-components - v0.0.205 - 2020-01-17
 Copyright (c) Ziggeo,Oliver Friedmann,Rashad Aliyev
 Apache-2.0 Software License.
 */

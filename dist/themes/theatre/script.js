5826f606ff697f8e4de6ba4253623d29105c4c3e
Oliver Friedmann <oliver.friedmann@gmail.com>
2020-04-14 11:24:43 -0400

    Merge

--- dist/themes/theatre/script.js
+++ dist/themes/theatre/script.js
@@ -1,5 +1,5 @@
 /*!
-betajs-media-components - v0.0.219 - 2020-04-13
+betajs-media-components - v0.0.220 - 2020-04-14
 Copyright (c) Ziggeo,Oliver Friedmann,Rashad Aliyev
 Apache-2.0 Software License.
 */

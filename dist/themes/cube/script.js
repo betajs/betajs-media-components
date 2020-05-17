fc954a72de4bab80f18d3fe6759d95d1d600421b
Oliver Friedmann <oliver.friedmann@gmail.com>
2020-05-17 01:09:27 -0400

    Merge

--- dist/themes/cube/script.js
+++ dist/themes/cube/script.js
@@ -1,5 +1,5 @@
 /*!
-betajs-media-components - v0.0.222 - 2020-04-30
+betajs-media-components - v0.0.224 - 2020-05-17
 Copyright (c) Ziggeo,Oliver Friedmann,Rashad Aliyev
 Apache-2.0 Software License.
 */

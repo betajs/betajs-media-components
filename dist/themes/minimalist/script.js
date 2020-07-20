b44d8a20f22e21558a88368488d9a154ac49511d
Oliver Friedmann <oliver.friedmann@gmail.com>
2020-07-19 23:16:07 -0400

    Merge

--- dist/themes/minimalist/script.js
+++ dist/themes/minimalist/script.js
@@ -1,5 +1,5 @@
 /*!
-betajs-media-components - v0.0.234 - 2020-06-28
+betajs-media-components - v0.0.235 - 2020-07-19
 Copyright (c) Ziggeo,Oliver Friedmann,Rashad Aliyev
 Apache-2.0 Software License.
 */

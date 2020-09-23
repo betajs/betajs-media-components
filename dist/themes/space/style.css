3780b9b1bf014c12d58aadd5e20548c90c837b50
Oliver Friedmann <oliver.friedmann@gmail.com>
2020-09-23 17:31:57 +0200

    Merge

--- dist/themes/space/style.css
+++ dist/themes/space/style.css
@@ -1,5 +1,5 @@
 /*!
-betajs-media-components - v0.0.241 - 2020-09-01
+betajs-media-components - v0.0.242 - 2020-09-23
 Copyright (c) Ziggeo,Oliver Friedmann,Rashad Aliyev
 Apache-2.0 Software License.
 */

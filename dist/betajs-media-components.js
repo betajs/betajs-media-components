02b4a5562c01272c7968375b83f5b50668cb7ecc
ziggeo-commits <ziggeo-commits@ziggeo.com>
2021-03-24 20:42:08 -0400

    Merge

--- dist/betajs-media-components.js
+++ dist/betajs-media-components.js
@@ -1,5 +1,5 @@
 /*!
-betajs-media-components - v0.0.254 - 2021-03-21
+betajs-media-components - v0.0.256 - 2021-03-24
 Copyright (c) Ziggeo,Oliver Friedmann,Rashad Aliyev
 Apache-2.0 Software License.
 */
@@ -1010,7 +1010,7 @@ Public.exports();
 	return Public;
 }).call(this);
 /*!
-betajs-media-components - v0.0.254 - 2021-03-21
+betajs-media-components - v0.0.256 - 2021-03-24
 Copyright (c) Ziggeo,Oliver Friedmann,Rashad Aliyev
 Apache-2.0 Software License.
 */
@@ -1025,8 +1025,8 @@ Scoped.binding('dynamics', 'global:BetaJS.Dynamics');
 Scoped.define("module:", function () {
 	return {
     "guid": "7a20804e-be62-4982-91c6-98eb096d2e70",
-    "version": "0.0.254",
-    "datetime": 1616349589942
+    "version": "0.0.256",
+    "datetime": 1616632900232
 };
 });
 Scoped.assumeVersion('base:version', '~1.0.96');

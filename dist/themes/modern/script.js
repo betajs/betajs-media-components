fb33474a20ed108d876533cd900ba4665b09f5ef
ziggeo-commits <ziggeo-commits@ziggeo.com>
2021-04-06 13:36:51 -0400

    Merge

--- dist/themes/modern/script.js
+++ dist/themes/modern/script.js
@@ -1,5 +1,5 @@
 /*!
-betajs-media-components - v0.0.257 - 2021-04-01
+betajs-media-components - v0.0.258 - 2021-04-06
 Copyright (c) Ziggeo,Oliver Friedmann,Rashad Aliyev
 Apache-2.0 Software License.
 */

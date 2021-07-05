5b27b1debc67b72eb51f12e3e40d957bd867c6a8
ziggeo-commits <ziggeo-commits@ziggeo.com>
2021-07-05 16:42:54 -0400

    Merge

--- dist/betajs-media-components-noscoped.js
+++ dist/betajs-media-components-noscoped.js
@@ -1,5 +1,5 @@
 /*!
-betajs-media-components - v0.0.268 - 2021-06-27
+betajs-media-components - v0.0.269 - 2021-07-05
 Copyright (c) Ziggeo,Oliver Friedmann,Rashad Aliyev
 Apache-2.0 Software License.
 */
@@ -14,8 +14,8 @@ Scoped.binding('dynamics', 'global:BetaJS.Dynamics');
 Scoped.define("module:", function () {
 	return {
     "guid": "7a20804e-be62-4982-91c6-98eb096d2e70",
-    "version": "0.0.268",
-    "datetime": 1624828075127
+    "version": "0.0.269",
+    "datetime": 1625517753301
 };
 });
 Scoped.assumeVersion('base:version', '~1.0.96');
@@ -14110,7 +14110,7 @@ Scoped.define("module:AudioRecorder.Dynamics.Recorder", [
                     "allowedextensions": null,
                     "filesizelimit": null,
                     "display-timer": true,
-                    "visualeffectvisible": false,
+                    "visualeffectvisible": true,
                     "visualeffectsupported": false,
                     "visualeffectheight": null,
                     "visualeffectminheight": 120,
--- dist/themes/theatre/script.js
+++ dist/themes/theatre/script.js
@@ -1,5 +1,5 @@
 /*!
-betajs-media-components - v0.0.268 - 2021-06-27
+betajs-media-components - v0.0.269 - 2021-07-05
 Copyright (c) Ziggeo,Oliver Friedmann,Rashad Aliyev
 Apache-2.0 Software License.
 */

5d978a767c740ef4a6784a8db46a69cdc66b65ac
ziggeo-commits <ziggeo-commits@ziggeo.com>
2021-10-03 15:16:23 -0400

    Merge

--- dist/themes/space/style.css
+++ dist/themes/space/style.css
@@ -1,5 +1,5 @@
 /*!
-betajs-media-components - v0.0.282 - 2021-09-29
+betajs-media-components - v0.0.283 - 2021-10-03
 Copyright (c) Ziggeo,Oliver Friedmann,Rashad Aliyev
 Apache-2.0 Software License.
 */

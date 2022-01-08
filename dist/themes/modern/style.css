b482c0630386ef2e74654ff01354413c9f43a032
ziggeo-commits <ziggeo-commits@ziggeo.com>
2022-01-08 15:42:17 -0500

    chore: merge

--- dist/themes/modern/style.css
+++ dist/themes/modern/style.css
@@ -1,5 +1,5 @@
 /*!
-betajs-media-components - v0.0.290 - 2021-12-05
+betajs-media-components - v0.0.291 - 2022-01-08
 Copyright (c) Ziggeo,Oliver Friedmann,Rashad Aliyev
 Apache-2.0 Software License.
 */

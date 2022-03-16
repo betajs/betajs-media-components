7c5eb8f4f0e0dd839b6f9896cf223ecf8032f72e
ziggeo-commits <ziggeo-commits@ziggeo.com>
2022-03-16 14:39:36 -0400

    chore: merge

--- dist/themes/cube/style.css
+++ dist/themes/cube/style.css
@@ -1,5 +1,5 @@
 /*!
-betajs-media-components - v0.0.294 - 2022-03-02
+betajs-media-components - v0.0.295 - 2022-03-16
 Copyright (c) Ziggeo,Oliver Friedmann,Rashad Aliyev
 Apache-2.0 Software License.
 */

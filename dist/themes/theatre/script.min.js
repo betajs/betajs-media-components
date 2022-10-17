cadb70bfe1377994b45600653b32d9b122bafa48
ziggeo-commits <ziggeo-commits@ziggeo.com>
2022-10-16 20:54:54 -0400

    chore: merge

--- dist/themes/theatre/script.min.js
+++ dist/themes/theatre/script.min.js
@@ -1,5 +1,5 @@
 /*!
-betajs-media-components - v0.0.322 - 2022-10-05
+betajs-media-components - v0.0.323 - 2022-10-16
 Copyright (c) Ziggeo,Oliver Friedmann,Rashad Aliyev
 Apache-2.0 Software License.
 */

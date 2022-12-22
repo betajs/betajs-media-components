c9c83a90c74813018972f484e1df282beaf704e2
ziggeo-commits <ziggeo-commits@ziggeo.com>
2022-12-22 13:49:44 -0500

    chore: merge

--- dist/themes/modern/script.min.js
+++ dist/themes/modern/script.min.js
@@ -1,5 +1,5 @@
 /*!
-betajs-media-components - v0.0.336 - 2022-12-15
+betajs-media-components - v0.0.338 - 2022-12-22
 Copyright (c) Ziggeo,Oliver Friedmann,Rashad Aliyev
 Apache-2.0 Software License.
 */

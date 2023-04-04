2419043b6e18bdfa0981bc466140c7ccaa3e90d1
Natasha Calleia <natasha@calleia.com.br>
2023-04-03 21:36:09 -0300

    Build v0.0.358

--- dist/themes/modern/script.min.js
+++ dist/themes/modern/script.min.js
@@ -1,5 +1,5 @@
 /*!
-betajs-media-components - v0.0.356 - 2023-03-30
+betajs-media-components - v0.0.358 - 2023-04-03
 Copyright (c) Ziggeo,Oliver Friedmann,Rashad Aliyev
 Apache-2.0 Software License.
 */

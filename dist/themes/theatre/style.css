0cf3111ca77edc71dfea6b25792173ebd27d6fe3
ziggeo-commits <ziggeo-commits@ziggeo.com>
2023-04-06 15:04:17 -0400

    chore: merge

--- dist/themes/theatre/style.css
+++ dist/themes/theatre/style.css
@@ -1,5 +1,5 @@
 /*!
-betajs-media-components - v0.0.361 - 2023-04-06
+betajs-media-components - v0.0.362 - 2023-04-06
 Copyright (c) Ziggeo,Oliver Friedmann,Rashad Aliyev
 Apache-2.0 Software License.
 */

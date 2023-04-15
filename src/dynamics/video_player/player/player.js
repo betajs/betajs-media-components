726984e567bfc808a8afc239f8a161df18095691
Natasha Calleia <natasha@calleia.com.br>
2023-04-15 01:37:44 -0300

    Create adtagurlfallbacks attribute

--- src/dynamics/video_player/player/player.js
+++ src/dynamics/video_player/player/player.js
@@ -160,6 +160,7 @@ Scoped.define("module:VideoPlayer.Dynamics.Player", [
                         "imasettings": {},
                         "adtagurl": null,
                         "adchoiceslink": null,
+                        "adtagurlfallbacks": null,
                         "inlinevastxml": null,
                         "adsposition": null,
                         "vmapads": false, // VMAP ads will set pre, mid, post positions inside XML file
@@ -379,6 +380,7 @@ Scoped.define("module:VideoPlayer.Dynamics.Player", [
                     "posterfitstrategy": "string",
                     "adtagurl": "string",
                     "adchoiceslink": "string",
+                    "adtagurlfallbacks": "array",
                     "inlinevastxml": "string",
                     "imasettings": "jsonarray",
                     "adsposition": "string",

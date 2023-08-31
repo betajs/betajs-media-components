654814d58a173e3a4d2616127c4b11f538cc76ff
Rashad Aliyev <sambua@users.noreply.github.com>
2023-08-31 17:42:18 +0400

    Update src/dynamics/video_player/player/states.js
    Co-authored-by: Natasha Calleia <5845648+ntshcalleia@users.noreply.github.com>

--- src/dynamics/video_player/player/states.js
+++ src/dynamics/video_player/player/states.js
@@ -481,12 +481,7 @@ Scoped.define("module:VideoPlayer.Dynamics.PlayerStates.LoadAds", [
                     this.listenOn(this.dyn.channel("ads"), "ad-error", function() {
                         if (this.dyn.get("outstream")) {
                             this.dyn.hidePlayerContainer();
-                            if ((this.dyn.get("nextadtagurls") && this.dyn.get("nextadtagurls").length > 0) || (this.dyn.get("adtagurlfallbacks") && this.dyn.get("adtagurlfallbacks").length > 0)) {
-                                this.dyn.set("adtagurl", this.dyn.get("nextadtagurls").length > 0 ? this.dyn.get("nextadtagurls").shift() : this.dyn.get("adtagurlfallbacks").shift());
-                                this.dyn.scopes.adsplayer.execute("requestAds");
-                            } else {
-                                this.dyn.setNextOutstreamAdTagURL(false, this);
-                            }
+                            this.dyn.setNextOutstreamAdTagURL(false, this);
                         } else if (this.dyn.get("adtagurlfallbacks") && this.dyn.get("adtagurlfallbacks").length > 0) {
                             this.dyn.set("adtagurl", this.dyn.get("adtagurlfallbacks").shift());
                             this.dyn.scopes.adsplayer.execute("requestAds");

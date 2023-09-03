0000ba6b8121389a62aa16d095004d7496faac0d
Rashad Aliyev <aliyev.resad@gmail.com>
2023-09-03 13:08:23 +0400

    Fix pausing ad if it's playing

--- src/dynamics/video_player/player/player.js
+++ src/dynamics/video_player/player/player.js
@@ -1649,7 +1649,8 @@ Scoped.define("module:VideoPlayer.Dynamics.Player", [
 
                         if (this.get('disablepause')) return;
 
-                        if (this.get("playing_ad")) this.scopes.adsplayer.execute("pause");
+                        if (this.get("playing_ad") || this.get("adsplaying"))
+                            this.scopes.adsplayer.execute("pause");
 
                         if (this.get("playing")) {
                             if (this.player && this.get("broadcasting")) {

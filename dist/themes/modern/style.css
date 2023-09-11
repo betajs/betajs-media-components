ed5a13624a3ec055dae9f588a9383e0d5767b4d5
Natasha Calleia <natasha@calleia.com.br>
2023-09-11 12:04:28 -0300

    Build v0.0.400

--- dist/themes/modern/style.css
+++ dist/themes/modern/style.css
@@ -1,5 +1,5 @@
 /*!
-betajs-media-components - v0.0.399 - 2023-09-06
+betajs-media-components - v0.0.400 - 2023-09-11
 Copyright (c) Ziggeo,Oliver Friedmann,Rashad Aliyev
 Apache-2.0 Software License.
 */

bd5838fbc02c37ea343ca9679518244562646285
Natasha Calleia <natasha@calleia.com.br>
2023-10-13 07:14:50 -0300

    Build v0.0.409

--- dist/themes/cube/style.css
+++ dist/themes/cube/style.css
@@ -1,5 +1,5 @@
 /*!
-betajs-media-components - v0.0.408 - 2023-10-11
+betajs-media-components - v0.0.409 - 2023-10-13
 Copyright (c) Ziggeo,Oliver Friedmann,Rashad Aliyev
 Apache-2.0 Software License.
 */

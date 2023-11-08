717af47c9b7612701c519eb42b4587fe01159b89
Rashad Aliyev <aliyev.resad@gmail.com>
2023-11-08 13:59:30 +0400

    Merging with master

--- dist/themes/theatre/style.css
+++ dist/themes/theatre/style.css
@@ -1,5 +1,5 @@
 /*!
-betajs-media-components - v0.0.418 - 2023-11-07
+betajs-media-components - v0.0.418 - 2023-11-08
 Copyright (c) Ziggeo,Oliver Friedmann,Rashad Aliyev
 Apache-2.0 Software License.
 */

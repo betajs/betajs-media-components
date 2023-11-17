d772328b5c06a841c70e54f980d23d8cc076f88d
Natasha Calleia <natasha@calleia.com.br>
2023-11-17 15:05:31 -0300

    Fix setTimeout delay

--- src/dynamics/ads_player/ads_player.js
+++ src/dynamics/ads_player/ads_player.js
@@ -50,7 +50,7 @@ Scoped.define("module:Ads.Dynamics.Player", [
                         if (volume > 0 && this.get("unmuteonclick")) {
                             return setTimeout(function() {
                                 this.adsManager.setVolume(Maths.clamp(volume, 0, 1));
-                            }.bind(this));
+                            }.bind(this), 200);
                         } else {
                             return this.adsManager.setVolume(Maths.clamp(volume, 0, 1));
                         }

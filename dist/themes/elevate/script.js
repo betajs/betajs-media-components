7d79165e9c29b7cd8a400c45927cae8c5298b177
Natasha Calleia <natasha@calleia.com.br>
2023-11-29 11:51:18 -0300

    Build v0.0.424

--- dist/themes/elevate/script.js
+++ dist/themes/elevate/script.js
@@ -1,5 +1,5 @@
 /*!
-betajs-media-components - v0.0.423 - 2023-11-20
+betajs-media-components - v0.0.424 - 2023-11-29
 Copyright (c) Ziggeo,Oliver Friedmann,Rashad Aliyev
 Apache-2.0 Software License.
 */

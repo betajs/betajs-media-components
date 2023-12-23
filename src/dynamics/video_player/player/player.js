73a9ed2ca943b5ba9ab9c140a7db260abb7bcb52
Rashad Aliyev <aliyev.resad@gmail.com>
2023-12-24 00:00:43 +0400

    minor fixes, replacing variable name

--- src/dynamics/video_player/player/player.js
+++ src/dynamics/video_player/player/player.js
@@ -789,7 +789,7 @@ Scoped.define("module:VideoPlayer.Dynamics.Player", [
                         styles = {
                             aspectRatio: aspectRatio
                         };
-                        if (showSidebar) styles.aspectRatio = this.get("sidebaroptions.aspectratio") || 838 / 360;
+                        if (gallerySidebar) styles.aspectRatio = this.get("sidebaroptions.aspectratio") || 838 / 360;
                         if (height) styles.height = isNaN(height) ? height : parseFloat(height).toFixed(2) + "px";
                         if (width) styles.width = isNaN(width) ? width : parseFloat(width).toFixed(2) + "px";
                         containerStyles = floatingonly ? {} : Objs.extend({}, styles);

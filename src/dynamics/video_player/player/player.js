4f385538a1eb0f1186eb52a11179e2950c5741f5
Rashad Aliyev <aliyev.resad@gmail.com>
2024-02-13 19:29:40 +0400

    added testid attributes

--- src/dynamics/video_player/player/player.js
+++ src/dynamics/video_player/player/player.js
@@ -93,6 +93,7 @@ Scoped.define("module:VideoPlayer.Dynamics.Player", [
                         "csstopmessage": "",
                         "csscontrolbar": "",
                         "csstracks": "",
+                        "testid": "ba-testid",
                         "width": "",
                         "height": "",
                         "presetkey": null,

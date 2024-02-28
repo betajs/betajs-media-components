4a965b3d756bab01534d627093bb74bb6cb18cd7
Rashad Aliyev <aliyev.resad@gmail.com>
2024-02-28 17:50:40 +0400

    removed run:nanoserver as demo script was there

--- tests/playwright/config.js
+++ tests/playwright/config.js
@@ -39,7 +39,7 @@ module.exports = defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: 'http://localhost:5000',
+        baseURL: 'http://localhost:5050',
         ignoreHTTPSErrors: true,
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */

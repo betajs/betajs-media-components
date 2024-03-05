440c4e4e0b78f654687e3b0654dbc656827f1c18
Edmund Cheng <edmund.cheng@kargo.com>
2024-03-05 13:12:19 -0500

    include moredetailslink validation

--- src/dynamics/ads_player/ads_player.js
+++ src/dynamics/ads_player/ads_player.js
@@ -309,10 +309,9 @@ Scoped.define("module:Ads.Dynamics.Player", [
                     replay: function() {
                         this._replay();
                     },
-                    redirect_to_more_details: function() {
-                        const link = this.get("moredetailslink");
-                        if (link) {
-                            window.open(link, '_blank');
+                    redirect: function(moredetailslink) {
+                        if (moredetailslink) {
+                            window.open(moredetailslink, '_blank');
                         }
                     },
                     close: function() {

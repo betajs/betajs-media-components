9dc6203168f4593d75d3a7fe37c25ae2b53999ce
Rashad Aliyev <aliyev.resad@gmail.com>
2024-03-14 03:14:52 +0400

    adde unit tests for midroll positions

--- tests/tests/units/midroll-ads-specs.js
+++ tests/tests/units/midroll-ads-specs.js
@@ -6,14 +6,25 @@ const player = new BetaJS.MediaComponents.VideoPlayer.Dynamics.Player({
 	element: $("#visible-fixture").get(0),
 	attrs: {
 		adsposition: `mid[${interVal}*]`,
-		minadintervals: 5,
 	}
 });
+
+const reset = () => {
+	player.set("midrollads", []);
+	player.initMidRollAds();
+	// set duration after calling above function
+	player.set("duration", player.get("duration") + 10);
+}
 
 QUnit.test("test midroll ads positions count", assert => {
 	const playerDuration = 60;
 	player.initMidRollAds();
 	player.set("duration", playerDuration);
-	assert.gte(player.get("midrollads").length, Math.floor(playerDuration / interVal), "midroll ads count should be greater than or equal to the midroll ads positions count");
-	assert.eq(player.get("midrollads")[0], interVal, "first midroll ad should be at the first midroll position");
+	assert.equal(player.get("midrollads").length, Math.floor(playerDuration / interVal), "midroll ads count should be greater than or equal to the midroll ads positions count");
+	assert.equal(player.get("midrollads")[0]?.position, interVal, "first midroll ad should be at the first midroll position");
+
+	// Will check only mid
+	player.set(`adsposition`, 'pre,mid');
+	reset();
+	assert.notEqual(player.get("midrollads")[0]?.poster, Math.floor(playerDuration / 2), "mid-rolls should be in the duration middle");
 });

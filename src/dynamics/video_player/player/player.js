842165a99291ad60f716d691d3facc10f23ff9c0
Wei Wong <98759502+wwongkargo@users.noreply.github.com>
2024-07-03 14:34:02 +0100

    KVID-594: Support fetching Companion ad slots from Shadow DOM (#557)
    * Support fetching Halo Companion ad slots from Shadow DOM
    
    * Only search Companion ad slots in Shadow DOM if specified

--- src/dynamics/video_player/player/player.js
+++ src/dynamics/video_player/player/player.js
@@ -3189,13 +3189,19 @@ Scoped.define("module:VideoPlayer.Dynamics.Player", [
                         const {
                             selector,
                             id,
-                            adslotid
+                            adslotid,
+                            incontainer
                         } = location;
                         if (!selector || !(id || adslotid)) {
                             console.warn(`Please provide selector and adslotid for companion ad`);
                             return;
                         }
-                        const element = document.querySelector(selector);
+                        let element;
+                        if (!incontainer) {
+                            element = document.querySelector(selector);
+                        } else {
+                            element = this.activeElement().querySelector(selector);
+                        }
                         if (element) {
                             Objs.map(companionAds, function(ad) {
                                 const adContent = ad.data?.content;

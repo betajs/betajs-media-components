49537a30e36cdf2d66f278de1943b5c1cfd084b5
Thompson Yeboah <maxwemade@gmail.com>
2024-08-27 17:12:09 +0100

    zig-5165 NextWidget can not be turned off when sidebar gallery is shown

--- src/dynamics/video_player/player/player.js
+++ src/dynamics/video_player/player/player.js
@@ -666,9 +666,9 @@ Scoped.define("module:VideoPlayer.Dynamics.Player", [
 
                             if (this.get("duration") >= minDurationNext && showNextTime && position > showNextTime && !this.get("next_active")) {
                                 this.set("next_active", true);
-                            }
-
-
+                            }        
+                            
+                            
                             if (this.get("duration") >= minDurationNext && position > engageTime && engageTime > 0) {
                                 this.channel("next").trigger("autoPlayNext");
                                 this.channel("next").trigger("playNext", true);

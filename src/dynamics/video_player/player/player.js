fd87303a526dba947a22bc8d39b6b520c51f9d49
Da Woon Lee <daniel.lee9415@gmail.com>
2024-09-20 14:15:55 -0700

    remove outstream clean up logic as it is handled in video-kargo-player now (#615)

--- src/dynamics/video_player/player/player.js
+++ src/dynamics/video_player/player/player.js
@@ -902,12 +902,6 @@ Scoped.define("module:VideoPlayer.Dynamics.Player", [
                             aspectRatio: aspectRatio
                         };
 
-                        // Outstream should not have a side bar and should always be floating.
-                        if (outstream) {
-                            this.set("floatingoptions.floatingonly", true);
-                            this.set("floatingoptions.sidebar", false);
-                        }
-
                         if (!fullscreened && gallerySidebar) styles.aspectRatio = this.get("sidebaroptions.aspectratio") || 838 / 360;
                         if (height) styles.height = isNaN(height) ? height : parseFloat(height).toFixed(2) + "px";
                         if (width) styles.width = isNaN(width) ? width : parseFloat(width).toFixed(2) + "px";

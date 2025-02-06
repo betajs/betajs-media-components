88d3448078562f304d8b26167f2910e03aafe631
Rashad Aliyev <aliyev.resad@gmail.com>
2025-02-06 18:32:48 +0400

    minor refactoring

--- src/dynamics/ads_player/ads_player.js
+++ src/dynamics/ads_player/ads_player.js
@@ -215,9 +215,7 @@ Scoped.define("module:Ads.Dynamics.Player", [
                         }
                     },
                     "ads:adCanPlay": function() {
-                        if (!this.get("adsplaying")) {
-                            this.set("adsplaying", true);
-                        }
+                        this.set("adsplaying", true);
                     },
                     "ads:ad-error": function() {
                         const fallbackUrl = this.parent()?.get('adtagurlfallbacks')
@@ -341,7 +339,9 @@ Scoped.define("module:Ads.Dynamics.Player", [
                     },
                     "ads:contentPauseRequested": function() {
                         // outstream ads will be visible on ads can play event
-                        this.set("adsplaying", !this.get("isoutstream"));
+                        if (!this.get("isoutstream")) {
+                            this.set("adsplaying", true)
+                        }
                         this.trackAdsPerformance(`ads-content-pause-requested`);
                     }
                 },
